e4d0bed810cec98b2e221e6d0dce7f1229810f6b
Portillo87 <wilfredovillata828@email.com>
2023-08-12 16:19:36 -0600

    Practica1

---
+++ U20230820_Practica1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace MyApp// Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+        {
+            //Declaracion de variables
+          string? nombre;
+          int edad;
+          string direcion;
+          int telefono; 
+          string? universidad;
+          string? carerra;
+          string? facultad;
+          int ciclo;
+
+        Console.WriteLine("\n*******Practica 1 - Progrmaccion estructurada******\n");
+        Console.WriteLine("Ingrese su nombre Completo:");//Obtener datos
+        nombre = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("\n")
+        
+         Console.WriteLine("Ingrese su nombre:");//Obtener datos
+        nombre = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("\n")
+         Console.WriteLine("Ingrese su nombre:");//Obtener datos
+        nombre = Convert.ToString(Console.ReadLine());
+
+         Console.WriteLine("Ingrese su nombre:");//Obtener datos
+        nombre = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("\n")
+
+         Console.WriteLine("Ingrese su nombre:");//Obtener datos
+        nombre = Convert.ToString(Console.ReadLine());
+         Console.WriteLine("\n")
+
+         Console.WriteLine("Ingrese su nombre:");//Obtener datos
+        nombre = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("\n")
+
+         Console.WriteLine("Ingrese su nombre:");//Obtener datos
+        nombre = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("\n")
+
+         Console.WriteLine("Ingrese su nombre:");//Obtener datos
+        nombre = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("\n")
+
+         Console.WriteLine("Ingrese su nombre:");//Obtener datos
+        nombre = Convert.ToString(Console.ReadLine());
+        Console.WriteLine("\n")
+
+
+        //inprimiendo datos
+        Console.WriteLine("Ingrese su nombre:");
+        Console.WriteLine("Ingrese su nombre:");
+        Console.WriteLine("Ingrese su nombre:");
+        Console.WriteLine("Ingrese su nombre:");
+        Console.WriteLine("Ingrese su nombre:");
+        Console.WriteLine("Ingrese su nombre:");
+        Console.WriteLine("Ingrese su nombre:");
+        Console.WriteLine("Ingrese su nombre:");
+        
+        }
+    }
+}
